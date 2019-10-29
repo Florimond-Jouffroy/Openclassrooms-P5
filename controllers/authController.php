ea6294827c0eb731d71b6b51e41b0e67d2ef1aa0
Florimond <florimond.jouffroy@gmail.com>
2019-10-29 14:50:02 +0100

    Connexion d'un utilisateur
    Création du formulaire de connexion
    Création des routes get et post
    Création des fonction dans le controlleur

--- controllers/authController.php
+++ controllers/authController.php
@@ -16,7 +16,76 @@ class AuthController extends Controller
         $this->userManager = new UserManager();
     }
 
-    
+    /**
+     * Affiche la page de connexion
+     */
+    public function login(){
+        return $this->render('login.html');
+    }
+
+    /**
+     * Permet de déconnecter l'utilisateur
+     */
+    public function deconexion()
+    {
+
+        if(isset($_SESSION['login'])){
+            $_SESSION = array();
+            session_destroy();
+
+            header('location: '.$this->url('home'));
+        }
+        else{
+            header('location: '.$this->url('home'));
+        }
+
+    }
+
+    /**
+     * Récupérer les champs du formulaire de connexion et gére la connexion de l'utilisateur
+     */
+    public function connexion()
+    {
+
+        // récupération des champ du formulaire de connexion
+        $login = $_POST['login'];
+        $password = $_POST['password'];
+
+        // création d'un utilisateur en fonction du login
+        $user = $this->userManager->getUserByLogin($login);
+
+
+        // vérification qu'il y a bien un utilisateur avec ce login
+        if($user == false)
+        {
+            $_SESSION['flash'] = 'Votre login n\'est pas bon !';
+            $_SESSION['flash_type'] = 'error';
+            header('location: '.$this->url('login'));
+        }
+        
+        // vérification que le mot de passe corespond bien a celui enregistrer dans la base
+        if(password_verify($password, $user->password() ))
+        {
+
+            $_SESSION['login'] = true;
+            $_SESSION['firstname'] = $user->firstname();
+            $_SESSION['lastname'] = $user->lastname();
+            $_SESSION['userId'] = $user->id();
+            $_SESSION['flash'] = 'Vous êtes bien connecté';
+            $_SESSION['flash_type'] = 'success';
+            $_SESSION['user_type'] = $user->user_type();
+            header('location: '.$this->url('home'));
+            exit;
+        }
+        else
+        {
+            $_SESSION['flash'] = 'Votre mot de passe n\'est pas correct';
+            $_SESSION['flash_type'] = 'error';
+            header('location: '.$this->url('login'));
+            exit;
+        }
+
+    }
 
 
 
--- routes/routes.php
+++ routes/routes.php
@@ -6,3 +6,6 @@ $router->get('home', ['controller'=>'HomeController', 'action'=>'home']);
 
 $router->get('inscription', ['controller'=>'UserController', 'action'=>'create']);
 $router->post('inscription', ['controller'=>'UserController', 'action'=>'store']);
+
+$router->get('login', ['controller'=>'AuthController', 'action'=>'login']);
+$router->post('login', ['controller'=>'AuthController', 'action'=>'connexion']);
